74c3c81866e28bb9e34235179e6e86742245eaf7
EUNJAESHIN <ejshin0214@gmail.com>
2017-08-14 15:59:46 +0900

    비밀번호 변경 구현 완료
    2ventModifyPassword.php

--- .gitconfig/app/src/main/java/com/example/win/a2vent/Activity_User_Info_Modify.java
+++ .gitconfig/app/src/main/java/com/example/win/a2vent/Activity_User_Info_Modify.java
@@ -2,7 +2,9 @@ package com.example.win.a2vent;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.AsyncTask;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +24,7 @@ public class Activity_User_Info_Modify extends Activity {
     ActivityUserInfoModifyBinding binding_UserInfoModify;
     Vibrator vibe;
     String pw1, pw2;
+    ModifyPW modifyPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +44,8 @@ public class Activity_User_Info_Modify extends Activity {
 
         if (pw1.equals(GlobalData.getUserPW()) && !pw2.equals(GlobalData.getUserPW())
                 && !binding_UserInfoModify.eTextModifyPw2.getText().toString().equals("")) {
-            toast = Toast.makeText(Activity_User_Info_Modify.this, "ㅇㅋ", Toast.LENGTH_SHORT);
-            toast.show();
+            modifyPW = new ModifyPW();
+            modifyPW.execute(GlobalData.getUserID(), pw1, pw2);
         } else if (!pw1.equals(GlobalData.getUserPW())) {
             binding_UserInfoModify.eTextModifyPw1.setText("");
             binding_UserInfoModify.eTextModifyPw1.setHint("현재 비밀번호가 일치하지 않음");
@@ -57,17 +60,79 @@ public class Activity_User_Info_Modify extends Activity {
             binding_UserInfoModify.eTextModifyPw2.setHint("변경할 비밀번호 미입력");
             vibe.vibrate(150);
         }
-
-    }
+    } // 비밀번호 변경
 
     public void onClick_ModifyCancle(View v) {
         this.finish();
-    }
+    } // 비밀번호 변경 취소
 
     @Override
     public void onBackPressed() {
         // super.onBackPressed();
+    } // 백키 비활성화
+
+    private class ModifyPW extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String serverURL = "2ventModifyPassword.php";
+
+            try {
+                ServerConnector serverConnector = new ServerConnector(serverURL);
+
+                serverConnector.addPostData("id", params[0]);
+                serverConnector.addPostData("pw1", params[1]);
+                serverConnector.addPostData("pw2", params[2]);
+
+                serverConnector.addDelimiter();
+                serverConnector.writePostData();
+                serverConnector.finish();
+
+                return serverConnector.response();
+
+            } catch (Exception e) {
+                Log.d("DB", "ModifyPW Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if (result.equals("변경 완료")) {
+                Intent intent_Modifydone = new Intent(Activity_User_Info_Modify.this, Activity_User_Login.class);
+                intent_Modifydone.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent_Modifydone);
+                toast = Toast.makeText(Activity_User_Info_Modify.this, "변경 완료", Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+                toast = Toast.makeText(Activity_User_Info_Modify.this, "Modify Error", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            Log.d("DB", "POST response - " + result);
+        }
+
+    } // 비밀번호 변경 AsyncTask
+
+    @Override
+    protected void onDestroy() {
+        if (modifyPW != null) {
+            modifyPW.cancel(true);
+        }
+        super.onDestroy();
     }
 
-
+    @Override
+    protected void onPause() {
+        if (modifyPW != null) {
+            modifyPW.cancel(true);
+        }
+        super.onPause();
+    }
 }
